fb308cfdb0a4fe098d2955b7e72ecc14597f376d
korobkioptom <45981185+korobkioptom@users.noreply.github.com>
2019-01-19 19:02:49 +0300

    Update botinok.py

--- botinok.py
+++ botinok.py
@@ -2,14 +2,37 @@ import requests
 
 bot_token = '710715631:AAHvw3WnM4W_weBbZPDoZyxr_Srk3K8y1zM'
 telegram = 'https://api.telegram.org/bot{}/'
-def get_updates (bot_token):
-	res = requests.get(telegram.format(bot_token))
-	return res.json()
-	pass
 
-def last_messaga (json_answer):
-	text_answer = json_answer['result'][-1]['message']['text']
-	chat_id = json_answer['result'][-1]['chat']['id']
-	return text_answer, chat_id
-	pass
-print  (last_messaga(get_updates(bot_token)))	
+def get_updates ():
+	res = requests.get(telegram.format(bot_token)+'getupdates')
+	#print (res.json())
+	return res.json()
+#print (get_updates())
+
+def last_messaga ():
+	updates = get_updates()
+	chat_id = updates['result'][-1]
+	text = chat_id['message']['text']
+	return text
+
+print  (last_messaga())	
+
+def post_messsaga (text_messagi):
+	chat_id_bot = '606375631'
+	otpravka = requests.post(telegram.format(bot_token)+'sendmessage?chat_id=606375631&text={}'.format(text_messagi))
+	return text_messagi
+post_messsaga('123bot')
+
+
+
+
+
+
+
+
+
+#def main():
+#	pass
+
+#if __name__ == '__main__':
+#	main()
